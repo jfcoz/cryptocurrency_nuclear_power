c32675bcbedecf263420b14a18eade4b03b6b18b
cocoz <cocoz@localhost>
2018-05-12 19:08:13 +0200

    first version

---
+++ README.md
@@ -0,0 +1,46 @@
+# cryptocurrency nuclear power
+
+This script calculate the total electricity power needed by cryptocurrency miners (only miners, not including air conditionner), via the network hashrate of commons cryptocurrency and the power of hardware needed to mine.
+
+On May 2018, cryptocurrency needs 11 nuclear reactors !
+
+~~~
+World electricity production (in 2013) : 2.67 TW
+One 900MW nuclear reactor average production : 694.44 MW
+Number of operated nuclear reactors in the world : 450
+
+          Power  : cryptocurrency
+     3201551583W : Bitcoin (3.20 GW)
+     1087954135W : Ethereum (1.09 GW)
+      501707581W : BitcoinCash (501.71 MW)
+      476119401W : Litecoin (476.12 MW)
+      440221590W : Dogecoin (440.22 MW)
+      365550551W : Myriad-Scrypt (365.55 MW)
+      206271707W : Crown (206.27 MW)
+      162860729W : Zcash (162.86 MW)
+      152638111W : Monacoin (152.64 MW)
+      138447978W : Viacoin (138.45 MW)
+      130149469W : Bulwark (130.15 MW)
+      114559915W : Sia (114.56 MW)
+      113067436W : Dash (113.07 MW)
+       58625548W : Nicehash-Equihash (58.63 MW)
+       55156108W : Monero (55.16 MW)
+       52159610W : Nicehash-NIST5 (52.16 MW)
+       51509604W : Nicehash-SHA (51.51 MW)
+       42726718W : Nicehash-Lyra2REv2 (42.73 MW)
+       36963518W : EthereumClassic (36.96 MW)
+...
+           5405W : Zetacoin (5.41 KW)
+           4901W : Creamcoin (4.90 KW)
+           3866W : Nicehash-X11Gost (3.87 KW)
+           2049W : Quark (2.05 KW)
+              1W : Influxcoin (1.39 W)
+
+Total used power for all listed cryptocurrency: 7.78 GW
+
+        = 0.29 % of world electricity
+        = 11.20 nuclear reactors of 900MW
+        = 2.49% of world nuclear reactors
+
+
+~~~
---
+++ cryptocurrency_nuclear_power.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use JSON;
+
+my $K=10**3;
+my $M=10**6;
+my $G=10**9;
+
+my $total_crypto_watts=0;
+
+# source : https://www.edf.fr/groupe-edf/espaces-dedies/l-energie-de-a-a-z/tout-sur-l-energie/produire-de-l-electricite/le-nucleaire-en-chiffres
+# La production mondiale d'électricité en 2013 a représenté un total de 23 405,7 TWh
+my $world_electricity_production_2013=23405 * 10**12 / 365 / 24;
+
+# Un réacteur de 900 MW produit en moyenne chaque mois 500 000 MWh, ce qui correspond à la consommation de 400 000 ménages environ.
+my $reactor_900MW_production_2013=500*$G / 30 / 24;
+
+# source : http://world-nuclear.org/information-library/current-and-future-generation/nuclear-power-in-the-world-today.aspx
+my $operable_nuclear_reactor=450;
+
+my @Sources=(
+  'https://whattomine.com/coins.json',
+  'https://whattomine.com/asic.json'
+);
+
+# Exclude due to bad nethash value
+my @ExcludesCoins=(
+  'Unobtanium',
+  'Myriad-SHA'
+);
+
+# From sugested miners on whattomine
+my $miners_efficiency={
+#  algo          => hash rate * unit / Watts
+# GPU
+  'Ethash'       =>     84    * $M   / 405,
+  'Groestl'      =>     63.9  * $M   / 450,
+  'PHI1612'      =>     45    * $M   / 390,
+  'CryptoNightV7'=>   2580    *  1   / 330,
+  'Equihash'     =>    870    *  1   / 360,
+  'Lyra2REv2'    =>     14.7  * $M   / 390,
+  'NeoScrypt'    =>   2460    * $K   / 450,
+  'TimeTravel10' =>     27    * $M   / 450,
+  'X16R'         =>     21    * $M   / 360,
+  'Skunkhash'    =>     54    * $M   / 345,
+  'NIST5'        =>     57    * $M   / 345,
+  'Xevan'        =>      4.8  * $M   / 360,
+# ASIC
+  'SHA-256'      =>  14000    * $G   / 1370,
+  'Scrypt'       =>   1000    * $M   / 1600,
+  'X11'          =>  34000    * $M   / 2100,
+  'Blake (2b)'   =>    815    * $G   / 1370,
+  'Quark'        =>   3300    * $M   / 120,
+  'Qubit'        =>   3300    * $M   / 130,
+  'Myr-Groestl'  =>      3.3  * $G   / 50,
+  'Skein'        =>      1.7  * $G   / 40,
+  'LBRY'         =>     20    * $G   / 200,
+  'Blake (14r)'  =>     80    * $G   / 205,
+  'Pascal'       =>     20    * $G   / 105,
+  'X11Gost'      =>      0.45 * $G   / 70,
+  'CryptoNight'  =>     55    * $K   / 140
+};
+
+my $crypto_stats={};
+
+my $ua=LWP::UserAgent->new();
+# LWP seems forbiden
+$ua->agent('curl/7.58.0');
+
+foreach my $url (@Sources) {
+  calculate_watts($url);
+}
+
+
+sub calculate_watts {
+  my $url=shift;
+
+  my $response=$ua->get($url);
+  die $response->status_line if (! $response->is_success);
+  
+  my $data=decode_json($response->decoded_content);
+  
+  foreach my $coin_name (keys(%{$data->{coins}})) {
+    next if (grep(/^\Q$coin_name\E$/,@ExcludesCoins));
+    my $coin_details=$data->{coins}->{$coin_name};
+    my $coin_algorithm=$coin_details->{algorithm};
+    my $coin_nethash=$coin_details->{nethash};
+    if (defined($miners_efficiency->{$coin_algorithm})) {
+      my $coin_watts=$coin_nethash/$miners_efficiency->{$coin_algorithm};
+      $crypto_stats->{$coin_name}=$coin_watts;
+      $total_crypto_watts+=$coin_watts;
+    } else {
+      die("Missing efficiency information for algorithm $coin_algorithm");
+    }
+  }
+
+}
+
+
+print "World electricity production (in 2013) : ".human_numbers($world_electricity_production_2013)."W\n";
+print "One 900MW nuclear reactor average production : ".human_numbers($reactor_900MW_production_2013)."W\n";
+print "Number of operated nuclear reactors in the world : $operable_nuclear_reactor\n";
+print "\n";
+
+
+printf ("%15s  : cryptocurrency\n",'Power');
+foreach my $coin_name (sort({$crypto_stats->{$b} <=> $crypto_stats->{$a}} keys(%$crypto_stats))) {
+  printf ("%15dW : %s (%sW)\n",$crypto_stats->{$coin_name}, $coin_name, human_numbers($crypto_stats->{$coin_name}));
+}
+
+print "\n";
+print "Total used power for all listed cryptocurrency: ".human_numbers($total_crypto_watts)."W\n";
+
+print "\n";
+my $crypto_world_power_ratio=$total_crypto_watts*100/$world_electricity_production_2013;
+print "\t= ".sprintf("%.2f",$crypto_world_power_ratio)." % of world electricity\n";
+
+my $nuclear_reactor_needed_for_cryptocurrency = $total_crypto_watts/$reactor_900MW_production_2013;
+print "\t= ".sprintf("%.2f",$nuclear_reactor_needed_for_cryptocurrency)." nuclear reactors of 900MW\n";
+
+my $cryptocurrency_percentage_of_all_nuclear_reactors=$nuclear_reactor_needed_for_cryptocurrency*100/$operable_nuclear_reactor;
+print "\t= ".sprintf("%.2f",$cryptocurrency_percentage_of_all_nuclear_reactors)."% of world nuclear reactors\n";
+
+sub human_numbers {
+  my $value=shift;
+  my $unit_pos=0;
+  my @units=('','K','M','G','T','P','E','Z','Y');
+  while ($value > 1000) {
+    $unit_pos++;
+    $value=$value/1000;
+  }
+  return sprintf('%.2f',$value).' '.$units[$unit_pos];
+}
